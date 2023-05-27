ee9ba57cd04a685dbc3ae255cb58ad1ada147369
Jadyn Gomez <jadyngg19@gmail.com>
2023-05-27 11:00:23 -0400

    Getting the random recipe function to work and setting up the sever

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
 const hbs = exphbs.create({ helpers });
 
@@ -20,6 +22,9 @@ const sess = {
   })
 };
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(session(sess));
 
 app.use(express.json());
@@ -28,5 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
