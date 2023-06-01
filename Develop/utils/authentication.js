867e695939921ebacaa1ff11b90b7b50ea97aa14
Tyler <tbobross5@optimum.net>
2023-06-01 18:52:09 -0400

    added code to redirect user to login page if not

---
+++ Develop/utils/authentication.js
@@ -0,0 +1,8 @@
+const withAuthenitcator= (req, res, next) => {
+    if(!req.session.logged_in){
+        res.redirect('/login');
+    } else{
+        next();
+    }
+}
+module.exports=withAuthenitcator;
