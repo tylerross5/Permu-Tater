6f4d0a2bf55956fde47624137f0fbc08964dd9f7
Will Riffe <w_riffe@protonmail.com>
2023-06-07 13:31:30 -0400

    📦️ Added/Edited files for random vs real recipes

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -3,8 +3,10 @@ const app=express();
 
 const recipeRoute= require('./recipeRoute');
 const userRoutes= require('./userRoutes');
+const pullRecipeRoute = require('./themealdbRoute')
 
 app.use(recipeRoute);
 app.use(userRoutes);
+app.use(pullRecipeRoute);
 
 module.exports = app;
---
+++ Develop/controllers/themealdbRoute.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const Recipe = require('../models/Recipe');
+const sequelize = require('../config/connection');
+
+const router = express.Router();
+
+router.get('/themealdb', async (req, res) => {
+  try {
+    // Make a request to the Free Meal API and retrieve the random recipe data
+    const response = await axios.get('https://www.themealdb.com/api/json/v1/1/random.php');
+
+    // Parse the JSON response and extract the necessary information
+    const recipe = response.data.meals[0];
+    const {
+      idMeal,
+      strMeal,
+      strCategory,
+      strArea,
+      strInstructions,
+      strMealThumb,
+      strTags,
+      strYoutube
+    } = recipe;
+
+    // Extract the ingredients and their measurements
+    const ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+      if (recipe[`strIngredient${i}`]) {
+        const ingredient = {
+          name: recipe[`strIngredient${i}`],
+          measurement: recipe[`strMeasure${i}`] || '' // Use an empty string if measurement is not provided
+        };
+        ingredients.push(ingredient);
+      }
+    }
+
+    // Create a new recipe object
+    const newRecipe = {
+      idMeal,
+      strMeal,
+      strCategory,
+      strArea,
+      strInstructions,
+      strMealThumb,
+      strTags,
+      strYoutube,
+      ingredients: JSON.stringify(ingredients) // Convert ingredients array to a JSON string
+    };
+
+    // Save the recipe to the MySQL database using Sequelize
+    await Recipe.create(newRecipe);
+
+    // Send the recipe data as the response
+    res.json(newRecipe);
+  } catch (error) {
+    console.error('Error retrieving random recipe data from the Free Meal API: ', error);
+    res.status(500).json({ error: 'Failed to retrieve random recipe data' });
+  }
+});
+
+module.exports = router;
