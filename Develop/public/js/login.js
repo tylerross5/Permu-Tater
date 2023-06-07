4a926baa7de1a7b02a251278358c332e0f29a9ac
knudsam <knudsam@gmail.com>
2023-06-07 12:23:43 -0400

    Updated html

--- Develop/public/js/login.js
+++ Develop/public/js/login.js
@@ -1,21 +1,18 @@
-// This will display a message saying "Welcome!" if the email is "example@example.com"
-// and the password is "password", and display a message saying "Incorrect email or password." otherwise.
-
 const loginForm = document.getElementById("login-form");
 
 loginForm.addEventListener("submit", (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  const email = document.getElementById("email-input").value;
-  const password = document.getElementById("password-input").value;
+    const email = document.getElementById("email-input").value;
+    const password = document.getElementById("password-input").value;
 
-  if (email === "example@example.com" && password === "password") {
-    alert("Welcome!");
-  } else {
-    alert("Incorrect email or password.");
-  }
-  // Refresh the page after 1 seconds
-  setTimeout(function () {
-    location.reload();
-  }, 1000);
+    if (email === "example@example.com" && password === "password") {
+        alert("Welcome!");
+    } else {
+        alert("Incorrect email or password.");
+    }
+
+    // Optionally, you can clear the form fields after submission
+    loginForm.reset();
 });
+
